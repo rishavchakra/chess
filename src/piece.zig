df7f429c2e8bfb1853d5267bc524c4eed503d7d0
rishavchakra <rishavchakra@gmail.com>
2023-09-07 20:00:24 -0500

    Piece implementation

---
+++ src/piece.zig
@@ -0,0 +1,41 @@
+pub const Piece = u8;
+
+pub const PieceType = enum(u8) {
+    None = 0b0,
+    Pawn = 0b1,
+    Bishop = 0b10,
+    Knight = 0b11,
+    Rook = 0b100,
+    Queen = 0b101,
+    King = 0b110,
+
+    pub fn getPieceType(p: Piece) PieceType {
+        return switch (p & 0b111) {
+            0 => PieceType.None,
+            0b001 => PieceType.Pawn,
+            0b010 => PieceType.Bishop,
+            0b011 => PieceType.Knight,
+            0b100 => PieceType.Rook,
+            0b101 => PieceType.Queen,
+            0b110 => PieceType.King,
+            else => unreachable,
+        };
+    }
+};
+
+pub const Side = enum(u8) {
+    White = 0b0000,
+    Black = 0b1000,
+
+    pub fn getPieceSide(p: Piece) Side {
+        return switch ((p >> 3) & 0b1) {
+            0 => Side.White,
+            1 => Side.Black,
+            else => unreachable,
+        };
+    }
+};
+
+pub fn pieceInit(pt: PieceType, side: Side) Piece {
+    return @intFromEnum(pt) | @intFromEnum(side);
+}
