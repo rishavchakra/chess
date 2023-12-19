d56a38a455c302ebe033b732fa44abe67abb57b5
rishavchakra <rishavchakra@gmail.com>
2023-12-18 23:37:01 -0600

    Bitboard directional fix

--- src/bitboard.zig
+++ src/bitboard.zig
@@ -1,7 +1,11 @@
 // MSB      ...     LSB
 // North    ...     South
 // Black    ...     White
+// Bitboards
 pub const Bitboard = u64;
+// Place bits are u64s with a single bit population.
+// They are meant to mark a single piece's position like a bitboard.
+pub const Placebit = u64;
 
 pub fn shiftNorth(board: Bitboard, comptime n: u8) Bitboard {
     return board << (8 * n);
@@ -12,9 +16,26 @@ pub fn shiftSouth(board: Bitboard, comptime n: u8) Bitboard {
 }
 
 pub fn shiftEast(board: Bitboard, comptime n: u8) Bitboard {
-    return board >> n;
+    return board << n;
 }
 
 pub fn shiftWest(board: Bitboard, comptime n: u8) Bitboard {
-    return board << n;
+    return board >> n;
 }
+
+pub fn shiftNE(board: Bitboard, comptime n: u8) Bitboard {
+    return board << (9 * n);
+}
+
+pub fn shiftSE(board: Bitboard, comptime n: u8) Bitboard {
+    return board >> (7 * n);
+}
+
+pub fn shiftSW(board: Bitboard, comptime n: u8) Bitboard {
+    return board >> (9 * n);
+}
+
+pub fn shiftNW(board: Bitboard, comptime n: u8) Bitboard {
+    return board << (7 * n);
+}
+
